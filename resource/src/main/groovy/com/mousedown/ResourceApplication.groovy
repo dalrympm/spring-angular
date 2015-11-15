f21efb349bbc9b416f52d16f7df58a5fae93db97
Mike Dalrymple <mike@watchwith.com>
2015-11-14 16:59:03 -0800

    Part 5 in progress...

--- resource/src/main/groovy/com/mousedown/ResourceApplication.groovy
+++ resource/src/main/groovy/com/mousedown/ResourceApplication.groovy
@@ -2,16 +2,14 @@ package com.mousedown
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @RestController
-@EnableRedisHttpSession
-class ResourceApplication extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+class ResourceApplication {
 
     static void main(String[] args) {
         SpringApplication.run ResourceApplication, args
@@ -20,12 +18,6 @@ class ResourceApplication extends WebSecurityConfigurerAdapter {
     @RequestMapping('/')
     def home() {
         [id: UUID.randomUUID().toString(), content: 'Hello World']
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable()
-        http.authorizeRequests().anyRequest().authenticated()
     }
 
 }
